252a02c65f7a551440a193fb08f0893f75c73c05
nganluu0903 <50857364+nganluu0903@users.noreply.github.com>
2022-03-22 03:56:22 +0700

    fix python

--- connect4.py
+++ connect4.py
@@ -34,6 +34,14 @@ async def echo(websocket, path):
         connected.remove(websocket)
 
 # Start the server
-start_server = websockets.serve(echo, "0.0.0.0", PORT)
+if on_heroku:
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(
+        pathlib.Path(__file__).with_name('localhost.pem'))
+
+    start_server = websockets.serve(
+        echo, "0.0.0.0", PORT, ssl=ssl_context)
+else:
+    start_server = websockets.serve(echo, "0.0.0.0", PORT)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
